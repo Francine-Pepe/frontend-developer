11e6a9c9361c3baa6963edd2f9f068b4be351edd
Francine <franmelopepe2@gmail.com>
2023-02-13 15:17:19 +0100

    data created. Here all the data from the page can be found

---
+++ src/data.tsx
@@ -0,0 +1,29 @@
+export const nav = [
+  {
+    name: "about",
+    link: "/about",
+    id: 0,
+  },
+  {
+    name: "projects",
+    link: "/projects",
+    id: 1,
+  },
+  {
+    name: "skills",
+    link: "/skills",
+    id: 2,
+  },
+  {
+    name: "contact",
+    link: "/contact",
+    id: 3,
+  },
+];
+
+export const aboutText = [
+    {
+        title: "About me",
+        text: "Frontend developer with a Design background - I have almost 10 years of experience in the Fashion Design area. I am an open-minded, creative, and hard-working person, who likes to work in groups ( which I think is very important for us to exchange experiences - I learned a lot working in groups, with different people each week ), but enjoys working by herself as well ( working alone I can organize better my ideas and thoughts - and listen to good music too. Who doesn't? ).My brief UI experience - while learning - led me to develop important and useful skills, that is, user-friendly, but always taking into account a beautiful and well-made Design."
+    }
+];
