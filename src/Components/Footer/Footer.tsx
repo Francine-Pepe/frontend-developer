fc0b891a6e46e986c68fd9079b72e52f5d664013
Francine <franmelopepe2@gmail.com>
2023-02-24 16:01:22 +0100

    footer component created

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.css";
+import Linkedin from "../Icons/Linkedin";
+import { GitHub } from "@mui/icons-material";
+import Github from "../Icons/Github";
+import Behance from "../Icons/Behance";
+import Instagram from "../Icons/Instagram";
+import Resumé from "../Icons/Resumé";
 
 function Footer() {
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  };
+  
   return (
-    <h1>Footer</h1>
-  )
+    <footer className={styles.footer_main_container}>
+      <section className={styles.footer_social_icons_container}>
+        <Linkedin />
+        <Github />
+        <Behance />
+        <Instagram />
+        <Resumé />
+      </section>
+      <section>
+        <small>&copy; Copyright {getCurrentYear()} - Francine Pêpe</small>
+      </section>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
