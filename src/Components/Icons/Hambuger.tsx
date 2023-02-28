41f5fea5cf899079e5ba1fadc22a57bbc82c72b2
Francine <franmelopepe2@gmail.com>
2023-02-28 15:13:42 +0100

    hamburger icon created

---
+++ src/Components/Icons/Hambuger.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
+function Hambuger() {
+  return (
+    <div>
+      <button>
+        <Icon
+          icon="charm:menu-hamburger"
+          color="#4d4d4d"
+          width="24"
+          height="24"
+        />
+      </button>
+    </div>
+  );
+}
+
+export default Hambuger;
