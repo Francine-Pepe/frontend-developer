b17cf11d2df68ec94a2fae456116fe8c6968d7b0
Francine <franmelopepe2@gmail.com>
2023-03-03 12:43:03 +0100

    skills data added

--- src/data.tsx
+++ src/data.tsx
@@ -19,6 +19,7 @@ import FreshFruit06 from "./Assets/FreshFruit/freshFruit06.png";
 import FreshFruit07 from "./Assets/FreshFruit/freshFruit07.png";
 import FreshFruit08 from "./Assets/FreshFruit/freshFruit08.png";
 import FreshFruit09 from "./Assets/FreshFruit/freshFruit09.png";
+import { Icon } from "@iconify/react";
 
 export const nav = [
   {
@@ -46,7 +47,7 @@ export const nav = [
 export const aboutText = [
   {
     title: "About me",
-    text: "Hi, my name is Francine and I am a Frontend Developer! I am currently based in Hamburg, Germany, but I was born in Brazil. I love to create stuff, and that is why I first worked as a Fashion Designer for almost 10 years. After moving to Germany, I felt like changing my career as well, but I still wanted to continue working creatively, so, here I am creating beautiful and functional Websites and Apps. I am passionate about Visual Design and Animation, which I guess I express myself in the most effective and clean way. And what do I do? You can find out bellow",
+    text: "Hi, my name is Francine and I am a Developer! I am currently based in Hamburg, Germany, but I was born in Brazil. I love to create stuff, and that is why I first worked as a Fashion Designer for almost 10 years. After moving to Germany, I felt like changing my career as well, but I still wanted to continue working creatively, so, here I am creating beautiful and functional Websites and Apps. I am passionate about Visual Design and Animation, which I guess I express myself in the most effective and clean way. And what do I do? You can find out bellow",
   },
 ];
 
@@ -103,7 +104,8 @@ export const ProjectsDataLeft = [
     route: "/project/freshfruit",
     visit: "https://francine-pepe.github.io/fresh-fruit-at-your-door/",
     repo: "https://github.com/Francine-Pepe/fresh-fruit-at-your-door",
-    description: "Fresh Fruit is a company that deliver Fruit monthly or weekly directly at your door. The main feature from this website is to update the shopping cart and filter the fruits by season"
+    description:
+      "Fresh Fruit is a company that deliver Fruit monthly or weekly directly at your door. The main feature from this website is to update the shopping cart and filter the fruits by season",
   },
   {
     id: 2,
@@ -112,7 +114,7 @@ export const ProjectsDataLeft = [
     route: "/bookmark",
     visit: "https://francine-pepe.github.io/bookmark/",
     repo: "https://github.com/Francine-Pepe/bookmark",
-    description: "Bookmark is a code chalenge developed by: frontendmentor.io"
+    description: "Bookmark is a code chalenge developed by: frontendmentor.io",
   },
   {
     id: 3,
@@ -153,31 +155,31 @@ export const ProjectsDataRight = [
 
 export const ProjectPage = [
   {
-    image: FreshFruit01
+    image: FreshFruit01,
   },
   {
-    image: FreshFruit02
+    image: FreshFruit02,
   },
   {
-    image: FreshFruit03
+    image: FreshFruit03,
   },
   {
-    image: FreshFruit04
+    image: FreshFruit04,
   },
   {
-    image: FreshFruit05
+    image: FreshFruit05,
   },
   {
-    image: FreshFruit06
+    image: FreshFruit06,
   },
   {
-    image: FreshFruit07
+    image: FreshFruit07,
   },
   {
-    image: FreshFruit08
+    image: FreshFruit08,
   },
   {
-    image: FreshFruit09
+    image: FreshFruit09,
   },
 ];
 
@@ -185,12 +187,10 @@ export const Buttons = [
   {
     buttonLink: "Live demo",
     buttonRepo: "Repo",
-  }
-]
+  },
+];
 
-export const ModalImages = [
-  Photo1, Photo2, Photo3, Photo4, Photo5, Photo6
-]
+export const ModalImages = [Photo1, Photo2, Photo3, Photo4, Photo5, Photo6];
 
 export const ModalCaption = [
   "When not coding, I am probably photographing somewhere or someone",
@@ -199,4 +199,135 @@ export const ModalCaption = [
   "And model too.",
   "A bit of my workspace.",
   "Colors and flowers. Yes, I do love.",
-]
+];
+
+export const skillsFrontend = [
+  {
+    id: 1,
+    image: <Icon icon="logos:html-5" width="40" height="40" />,
+    level: 90,
+    name: "HTML 5 - Advanced"
+  },
+  {
+    id: 2,
+    image: <Icon icon="logos:css-3" width="40" height="40" />,
+    level: 90,
+    name: "CSS 3 - Advanced"
+  },
+  {
+    id: 3,
+    image: <Icon icon="logos:javascript" width="40" height="40" />,
+    level: 70,
+    name: "JavaScript - Intermediate"
+  },
+  {
+    id: 4,
+    image: (
+      <Icon
+        icon="vscode-icons:file-type-typescript-official"
+        width="40"
+        height="40"
+      />
+    ),
+    level: 50,
+    name: "Typescript - Basic"
+  },
+  {
+    id: 5,
+    image: (
+      <Icon icon="vscode-icons:file-type-reactjs" width="40" height="40" />
+    ),
+    level: 80,
+    name: "React - Advanced"
+  },
+  {
+    id: 6,
+    image: (
+      <Icon
+        icon="simple-icons:chakraui"
+        color="#44c8c0"
+        width="40"
+        height="40"
+      />
+    ),
+    level: 70,
+    name: "Chakra UI - Advanced"
+  },
+  {
+    id: 7,
+    image: (
+      <Icon
+        icon="skill-icons:materialui-light"
+        color="#44c8c0"
+        width="40"
+        height="40"
+      />
+    ),
+    level: 70,
+    name: "Material UI - Advanced"
+  },
+  {
+    id: 8,
+    image: (
+      <Icon icon="logos:bootstrap" color="#44c8c0" width="40" height="40" />
+    ),
+    level: 50,
+    name: "Bootstrap - Basic"
+  },
+  {
+    id: 9,
+    image: (
+      <Icon icon="mdi:responsive" color="#707070" width="40" height="40" />
+    ),
+    level: 90,
+    name: "Responsiveness - Advanced"
+  },
+];
+
+export const skillsDesign = [
+  {
+    id: 10,
+    image: <Icon icon="logos:adobe-xd" width="40" height="40" />,
+    level: 90,
+    name: "Adobe XD - Advanced"
+  },
+  {
+    id: 11,
+    image: <Icon icon="logos:adobe-photoshop" width="40" height="40" />,
+    level: 40,
+    name: "Adobe Photoshop - Basic"
+  },
+  {
+    id: 12,
+    image: <Icon icon="logos:trello" width="40" height="40" />,
+    level: 70,
+    name: "Trello - Intermediate"
+  },
+  {
+    id: 13,
+    image: <Icon icon="logos:slack-icon" width="40" height="40" />,
+    level: 70,
+    name: "Slack - Intermediate"
+  },
+];
+
+export const skillsDevelopment = [
+  {
+    id: 14,
+    image: <Icon icon="logos:github-icon" width="40" height="40" />,
+    level: 70,
+    name: "Github - Intermediate"
+  },
+  {
+    id: 15,
+    image: <Icon icon="vscode-icons:file-type-vscode" width="40" height="40" />,
+    level: 70,
+    name: "VSCode - Intermediate"
+  },
+  {
+    id: 16,
+    image: <Icon icon="carbon:api-1" width="40" height="40" />,
+    level: 60,
+    name: "API - Intermediate"
+  },
+];
