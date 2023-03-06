9c4bbbfa20f61ec358d3debe30aa54333afa22a2
Francine <franmelopepe2@gmail.com>
2023-03-06 18:21:16 +0100

    animation props

---
+++ src/Components/Animations/Animation.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Animations.module.css";
+
+function ProjectAnimation(props: any) {
+  const { text, image } = props;
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.typing_demo}>{text}</div>
+      <div>{image}</div>
+    </div>
+  );
+}
+
+export default ProjectAnimation;
