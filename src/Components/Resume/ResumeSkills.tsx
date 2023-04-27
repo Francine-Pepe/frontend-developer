66946d6534b1c515f12c31915fdff6628b98222f
Francine <franmelopepe2@gmail.com>
2023-04-27 12:09:20 +0200

    refactor: arrray destructing

--- src/Components/Resume/ResumeSkills.tsx
+++ src/Components/Resume/ResumeSkills.tsx
@@ -3,31 +3,25 @@ import styles from "./Resume.module.css";
 import ResumeSkillsProps from "./ResumeSkillsProps";
 import { ResumeData, TechSkills } from "../../data";
 import ResumeProgressBar from "../ProgressBar/ResumeProgressBar";
+import ResumeTitles from "./ResumeTitles";
 
 function ResumeSkills() {
   return (
     <main>
       <section className={styles.resume_left_container}>
-        {ResumeData.map((data, index) => {
+        {ResumeData.map(({ tech }, index) => {
           return (
-            <>
-              <div className={styles.profile_data}>
-                <h3>{data.tech}</h3>
-                <hr />
-                {TechSkills.map(({ name, level }) => {
-                  return (
-                    <>
-                      <div className={styles.resume_left_container}>
-                        <div className={styles.skills_resume_left_container}>
-                          <ResumeSkillsProps name={name} />
-                          <ResumeProgressBar completed={level} />
-                        </div>
-                      </div>
-                    </>
-                  );
-                })}
-              </div>
-            </>
+            <div className={styles.profile_data} key={index}>
+              <ResumeTitles title={tech} />
+              {TechSkills.map(({ id, name, level }) => {
+                return (
+                  <div className={styles.skills_resume_left_container} key={id}>
+                    <ResumeSkillsProps name={name} />
+                    <ResumeProgressBar completed={level} />
+                  </div>
+                );
+              })}
+            </div>
           );
         })}
       </section>
