37e93e5a823a2d175194f3835ff4b029f932d87a
Francine <franmelopepe2@gmail.com>
2023-05-10 20:12:37 +0200

    projects fixes. Refactor: projects props component created.

---
+++ src/Components/Projects/ProjectsProps.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./Projects.module.css";
+
+function ProjectsProps(props: {
+  id: number;
+  image: any;
+  name: string;
+  visit: string;
+  repo: string;
+}) {
+  const { id, image, name, visit, repo } = props;
+  return (
+    <main>
+      <section className={styles.projects_props}>
+        <h3>{name}</h3>
+        <a href={visit} target="_blank" rel="noreferrer">
+          <img src={image} alt={name} key={id} />
+        </a>
+        <div className={styles.buttons_container}>
+          <a href={visit} target="_blank" rel="noreferrer">
+            <button>Live demo</button>
+          </a>
+          <a href={repo} target="_blank" rel="noreferrer">
+            <button>Repo</button>
+          </a>
+        </div>
+        <hr />
+      </section>
+    </main>
+  );
+}
+
+export default ProjectsProps;
