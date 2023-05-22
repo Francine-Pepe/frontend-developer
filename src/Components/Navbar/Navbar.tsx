954ccc152439f7190ad3ed141ee9e9a4af14ab63
Francine <franmelopepe2@gmail.com>
2023-05-22 14:23:44 +0200

    animation added to the navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,31 +1,33 @@
 import { NavLink } from "react-router-dom";
 import { nav } from "../../data";
 import styles from "./Navbar.module.css";
-import Logo from "../../Assets/Logo_FrancinePepe.png";
+import Logo from "../../Assets/FrancinePêpe_logo.png";
 import NavbarMobile from "./NavbarMobile";
 
 function Navbar() {
   return (
-    <nav className={styles.navbar_container}>
-      <NavLink to={"/"} className={styles.logo_link}>
-        <img src={Logo} alt={"Logo"} className={styles.logo} />
-      </NavLink>
+    <section className={styles.nav}>
+      <nav className={styles.navbar_container}>
+        <NavLink to={"/"} className={styles.logo_link}>
+          <img src={Logo} alt={"Logo"} className={styles.logo} />
+        </NavLink>
 
-      <div className={styles.navbar}>
-        <ul id={styles.list}>
-          {nav.map((data) => {
-            return (
-              <NavLink to={data.link} key={data.index}>
-                <li>{data.name}</li>
-              </NavLink>
-            );
-          })}
-        </ul>
-      </div>
-      <div className={styles.navbar_mobile_container}>
-        <NavbarMobile />
-      </div>
-    </nav>
+        <div className={styles.navbar}>
+          <ul >
+            {nav.map((data) => {
+              return (
+                <NavLink to={data.link} key={data.index} className={styles.item}>
+                  <li className={styles.linktext}>{data.name}</li>
+                </NavLink>
+              );
+            })}
+          </ul>
+        </div>
+        <div className={styles.navbar_mobile_container}>
+          <NavbarMobile />
+        </div>
+      </nav>
+    </section>
   );
 }
 
